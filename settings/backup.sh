01f62a0ef8b4feac00674b77d126561bd0595e20
Loic CHETY <lchety@e2r11p15.42.fr>
2018-06-08 16:04:33 +0200

    settings macos

---
+++ settings/backup.sh
@@ -0,0 +1,9 @@
+# Backup script for get settings on MacOS
+
+# Set preferences folder
+mkdir -p preferences
+
+# Set System Preferences
+cp -f ~/Library/Preferences/com.apple.dock.plist preferences/
+cp -f ~/Library/Preferences/com.apple.systempreferences.plist preferences/
+cp -f ~/Library/Preferences/com.apple.security.cloudkeychainproxy3.keysToRegister.plist preferences/
