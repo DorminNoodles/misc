be9defa611da4a873fb01a033b43549af64cc654
DorminNoodles <loic.chety@gmail.com>
2019-08-02 16:33:44 +0200

    Update keyboardRemap.sh

--- keyboardRemap.sh
+++ keyboardRemap.sh
@@ -1,1 +1,3 @@
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E2,"HIDKeyboardModifierMappingDst":0x7000000E7},{"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E2}]}';
+
+//https://developer.apple.com/library/archive/technotes/tn2450/_index.html
