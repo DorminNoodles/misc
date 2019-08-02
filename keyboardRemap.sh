00382f9b3a8269b4b08523bdc5f29127fba50a62
Loic CHETY <lchety@e1r8p11.42.fr>
2019-08-02 16:00:33 +0200

    wip

---
+++ keyboardRemap.sh
@@ -0,0 +1,1 @@
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E2,"HIDKeyboardModifierMappingDst":0x7000000E7},{"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E2}]}';
